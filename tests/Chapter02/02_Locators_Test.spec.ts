069e2f62f291b0c990a2574240ba6b5e0bdbb74e
BakkappaN <bakkappac@gmail.com>
2024-12-10 18:41:43 +0530

    Playwright with TypeScript Tutorial Full Course

---
+++ tests/Chapter02/02_Locators_Test.spec.ts
@@ -0,0 +1,33 @@
+// Import playwright module
+import { test, expect } from '@playwright/test'
+
+// Write a test
+test('Locators in Playwright', async ({ page }) => {
+    // Go to URL
+    await page.goto('https://github.com/BakkappaN/');
+
+    // GetByRole
+    await page.getByRole('link', { name: 'Sign in' }).click()
+
+    // GetBylabel
+    await page.getByLabel('Homepage', { exact: true }).first().click();
+
+    // GetByAltText
+    // await page.getByAltText("View BakkappaN's full-sized avatar").click();
+
+    // GetByTestId
+    // await page.getByTestId("projects").first().click();
+
+    // GetByText
+    // await page.getByText("Sign up").click();
+
+    // GetByPlaceholder, Xpath, CSSSelectors
+    await page.goto('https://www.youtube.com/@testerstalk');
+    await page.getByPlaceholder('Search').fill('cypress by testers talk');
+    await page.locator('input[name="search_query"]').first().fill('playwright typescript by testers talk');
+
+    // GetByTitle
+    await page.goto('https://www.google.com/');
+    await page.getByTitle('Search').fill('playwright javascript by testers talk');
+})
+
