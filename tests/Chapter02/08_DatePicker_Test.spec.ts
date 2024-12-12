4429617d77546e6447905e9dd0dd0f1bb89fd425
BakkappaN <bakkappac@gmail.com>
2024-12-13 00:37:13 +0530

    Playwright with TypeScript Tutorial Full Course

---
+++ tests/Chapter02/08_DatePicker_Test.spec.ts
@@ -0,0 +1,28 @@
+// Import playwright module
+import { test, expect } from '@playwright/test'
+
+// Write a test
+test('Selecting date value in playwright', async ({ page }) => {
+    // Go to URL
+    await page.goto('https://jqueryui.com/datepicker/');
+
+    // Hardcoded date
+    const iframe = page.frameLocator('[class="demo-frame"]');
+    //await iframe.locator('[id="datepicker"]').fill('12/15/2024');
+
+    // Selecting dynamic date
+    // await iframe.locator('[id="datepicker"]').click();
+    // await iframe.locator('.ui-datepicker-today').click();
+
+    // Selecting past date
+    // await iframe.locator('[id="datepicker"]').click();
+    // await iframe.locator('[title="Prev"]').click();
+    // await iframe.locator('text="15"').click();
+
+    // Selecting future date
+    await iframe.locator('[id="datepicker"]').click();
+    await iframe.locator('[title="Next"]').click();
+    await iframe.locator('text="15"').click();
+})
+
+
---
+++ tests/Chapter02/09_Assertions_Test.spec.ts
@@ -0,0 +1,30 @@
+// Import playwright module
+import { test, expect } from '@playwright/test'
+
+// Write a test
+test('Assertions in playwright', async ({ page }) => {
+    // Go to URL
+    await page.goto('https://www.youtube.com/');
+
+    // visible, editable, enabled, empty
+    await expect(page.getByPlaceholder('Search', {exact :true}).first()).toBeVisible();
+    await expect(page.getByPlaceholder('Search', {exact :true}).first()).toBeEditable();
+    await expect(page.getByPlaceholder('Search', {exact :true}).first()).toBeEnabled();
+    await expect(page.getByPlaceholder('Search', {exact :true}).first()).toBeEmpty();
+
+    // Verify URL, title, text, count, 
+    await page.getByPlaceholder('Search', {exact :true}).first().click();
+    await page.getByPlaceholder('Search', {exact :true}).first().fill('playwright by testers talk')
+    await page.getByPlaceholder('Search', {exact :true}).first().press('Enter');
+    await expect(page).toHaveURL('https://www.youtube.com/results?search_query=playwright+by+testers+talk');
+
+    await expect(page).toHaveTitle('playwright by testers talk - YouTube');
+
+    await expect(page.locator('span[id="title"]').first()).toHaveText('Latest posts from Testers Talk');
+
+    await expect(page.locator('span[id="title"]')).toHaveCount(4);
+
+    //await expect(page.locator('span[id="title"]')).toBeDisabled();
+})
+
+
