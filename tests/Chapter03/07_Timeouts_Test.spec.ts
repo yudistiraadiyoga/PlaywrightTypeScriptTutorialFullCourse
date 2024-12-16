a21b7d96fdb987f932b66e55314513856b946785
BakkappaN <bakkappac@gmail.com>
2024-12-16 23:04:42 +0530

    Playwright with TypeScript Tutorial Full Course

---
+++ tests/Chapter03/07_Timeouts_Test.spec.ts
@@ -0,0 +1,21 @@
+// Import playwright module
+import { test, expect } from '@playwright/test'
+
+// Write a test
+test('Timeouts in Playwright ', async ({ page }) => {
+    test.setTimeout(1 * 60 * 1000);
+    // Go to URL
+    await page.goto('https://www.google.com/');
+
+    // Search with keywords
+    await page.getByLabel('Search', { exact: true }).fill('playwright by testers talk');
+    await page.getByLabel('Search', { exact: true }).press('Enter');
+
+    // Click on playlist
+    await page.getByRole('link', { name: 'Playwright by Testers Talk☑️' }).first().click({timeout: 5000});
+
+    // Validate web page title 
+    await expect(page).toHaveTitle('Playwright by Testers Talk☑️ - YouTube', {timeout : 5000});
+
+   // await page.waitForTimeout(60000);
+})
