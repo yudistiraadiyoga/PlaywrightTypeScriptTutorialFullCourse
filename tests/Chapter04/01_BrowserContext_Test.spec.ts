0ab312e0ee06deafff4fede155d277424132f78d
BakkappaN <bakkappac@gmail.com>
2024-12-17 23:42:38 +0530

    Playwright with TypeScript Tutorial Full Course

---
+++ tests/Chapter04/01_BrowserContext_Test.spec.ts
@@ -0,0 +1,44 @@
+// Import playwright module
+import { test, expect } from '@playwright/test'
+
+// Write a test
+test('Multiple browser/tabs in Playwright TypeScript', async ({ page, browser }) => {
+    // Go to URL
+    await page.goto('https://www.google.com/');
+
+    // Search with keywords
+    await page.getByLabel('Search', { exact: true }).fill('playwright by testers talk');
+    await page.getByLabel('Search', { exact: true }).press('Enter');
+
+    // Click on playlist
+    await page.getByRole('link', { name: 'Playwright by Testers Talk☑️' }).first().click();
+
+    // Validate web page title 
+    await expect(page).toHaveTitle('Playwright by Testers Talk☑️ - YouTube');
+
+
+    // New browser session
+    const context2 = await browser.newContext();
+    const page2 = await context2.newPage();
+    // Go to URL
+    await page2.goto('https://www.google.com/');
+
+    // Search with keywords
+    await page2.getByLabel('Search', { exact: true }).fill('playwright by testers talk');
+    await page2.getByLabel('Search', { exact: true }).press('Enter');
+
+    // Click on playlist
+    await page2.getByRole('link', { name: 'Playwright by Testers Talk☑️' }).first().click();
+
+    // Validate web page title 
+    await expect(page2).toHaveTitle('Playwright by Testers Talk☑️ - YouTube');
+
+
+    // Create new tabs
+    const newTab = await context2.newPage();
+    await newTab.goto('https://www.google.com/');
+
+    // Search with keywords
+    await newTab.getByLabel('Search', { exact: true }).fill('playwright by testers talk');
+    await newTab.getByLabel('Search', { exact: true }).press('Enter');
+})
---
+++ tests/Chapter04/02_ReRunFailed_Test.spec.ts
@@ -0,0 +1,28 @@
+// Import playwright module
+import { test, expect } from '@playwright/test'
+
+// Write a test
+test('Test 1', async ({ page }) => {
+    // Go to URL
+    await page.goto('https://www.google.com/');
+
+    // Search with keywords
+    await page.getByLabel('Search', { exact: true }).fill('playwright by testers talk');
+    await page.getByLabel('Search', { exact: true }).press('Enter');
+
+    // Click on playlist
+    await page.getByRole('link', { name: 'Playwright by Testers Talk☑️' }).first().click();
+
+    // Validate web page title 
+    await expect(page).toHaveTitle('Playwright by Testers Talk☑️ - YouTube');
+})
+
+// Write a test
+test('Test 2', async ({ page }) => {
+   expect(true).toBe(false);
+})
+
+// Write a test
+test('Test 3', async ({ page }) => {
+    expect(true).toBe(false);
+})
