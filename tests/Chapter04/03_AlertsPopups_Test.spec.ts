2af3797ba5331a14f86e5b01068953e1057d94c4
BakkappaN <bakkappac@gmail.com>
2024-12-20 18:15:55 +0530

    Playwright with TypeScript Tutorial Full Course

---
+++ tests/Chapter04/03_AlertsPopups_Test.spec.ts
@@ -0,0 +1,44 @@
+// Import playwright module
+import { test, expect } from '@playwright/test'
+
+// Write a test
+test('Handling Alert popups in Playwright ', async ({ page }) => {
+    // Go to URL
+    await page.goto('https://www.selenium.dev/documentation/webdriver/interactions/alerts/');
+
+    page.once('dialog', dialog =>{
+        dialog.accept();
+        console.log(`Alert message is : ${dialog.message()}`);
+        console.log(`Dialog type is : ${dialog.type()}`);
+    })
+    await page.getByText('See an example alert', {exact : true}).click();
+})
+
+// Write a test
+test('Handling Popups in Playwright ', async ({ page }) => {
+    // Go to URL
+    await page.goto('https://www.selenium.dev/documentation/webdriver/interactions/alerts/');
+
+    page.once('dialog', dialog =>{
+        console.log(`Dialog type is : ${dialog.type()}`);
+        //dialog.accept();
+        dialog.dismiss();
+        console.log(`Popup message is : ${dialog.message()}`);
+    })
+    await page.getByText('See a sample confirm', {exact : true}).click();
+})
+
+// Write a test
+test('Handling Prompt Popups in Playwright ', async ({ page }) => {
+    // Go to URL
+    await page.goto('https://www.selenium.dev/documentation/webdriver/interactions/alerts/');
+
+    page.once('dialog', async(dialog) =>{
+        console.log(`Dialog type is : ${dialog.type()}`);
+        console.log(`Prompt popup message is : ${dialog.message()}`);
+        await dialog.accept('playwright');
+    })
+    await page.getByText('See a sample prompt', {exact : true}).click();
+})
+
+
