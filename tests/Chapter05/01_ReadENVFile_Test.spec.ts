cd24d5a2b7f21475f6c4f74d7991f4ff09b9bb2d
BakkappaN <bakkappac@gmail.com>
2024-12-22 23:51:58 +0530

    Playwright with TypeScript Tutorial Full Course

---
+++ tests/Chapter05/01_ReadENVFile_Test.spec.ts
@@ -0,0 +1,20 @@
+// Import playwright module
+import { test, expect } from '@playwright/test'
+
+/**
+ * Testers Talk
+ */
+test('Read ENV file config in playwright', async ({ page }) => {
+    // Go to URL
+    await page.goto(`${process.env.GOOGLE_URL}`);
+
+    // Search with keywords
+    await page.getByLabel('Search', { exact: true }).fill(`${process.env.SEARCH_KEYWORDS}`);
+    await page.getByLabel('Search', { exact: true }).press('Enter');
+
+    // Click on playlist
+    await page.getByRole('link', { name: `${process.env.SEARCH_KEYWORDS}` }).first().click();
+
+    // Validate web page title 
+    await expect(page).toHaveTitle(`${process.env.SEARCH_KEYWORDS}`+'☑️ - YouTube');
+})
