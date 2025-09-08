b49d4b7301ac31a3401f7e7a39e8bd6d5f510bc6
yudistiraadiyoga <yudistira.adiyoga@ti.ukdw.ac.id>
2025-09-08 11:32:09 +0700

    a

--- playwright.config.ts
+++ playwright.config.ts
@@ -87,10 +87,10 @@ export default defineConfig({
     // },
 
     /* Test against branded browsers. */
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
     {
       name: 'Google Chrome',
       use: {
